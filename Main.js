c9c1ca71bf396876b60e4e5b7751661a1fdd02ef
ZestySalad <Baileylunsford@hotmail.com>
2024-08-17 01:53:57 -0500

    Assingment for Module 1 Hello World program

---
+++ Main.js
@@ -0,0 +1,9 @@
+console.log("Bailey Lunsford");
+console.log("1995");
+console.log(1995);
+console.log("Bailey", "Lunsford", "1995");
+console.log("Bailey");
+console.log("Lunsford");
+console.log("1995");
+console.log("Bailey"+"Lunsofrd"+"1995");
+console.log("Bailey ","Lunsford ", "1995");
